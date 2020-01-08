33fbca00c968f9dd750b925e812bdd675db26697
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-01-07 22:43:22 -0800

    Added re library

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from regexQuery import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Show homepage"""
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    """"""
+    regex = request.form['regex']
+    test_str = request.form['test_str']
+    matches, indices = regex_query(regex, test_str)
+    return render_template('result.html', matches=matches, indices=indices, test_str=test_str)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ regexQuery.py
@@ -0,0 +1,25 @@
+import re
+
+def regex_query(regex, test_string):
+    """"""
+    list_tuples_indices = []
+    matches = re.finditer(regex, test_string, re.MULTILINE)
+
+    matchNums = 0
+    for matchNum, match in enumerate(matches, start=1):
+        print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+        tuple_start_end_indices = match.start(), match.end()
+        list_tuples_indices.append(tuple_start_end_indices)
+
+        # for groupNum in range(0, len(match.groups())):
+        #     groupNum = groupNum + 1
+        #
+        #     print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+        matchNums = matchNum
+
+    return matchNums, list_tuples_indices
+
+regex = r"([A-Z])\w+"
+
+test_str = ("Lorem ipsum dolor sit amet, nam no malis graecis incorrupte. Dicunt insolens sed ex, ut saepe quaerendum eam. Eum no quas tempor impetus, in per expetendis reprimique mediocritatem. Meis erant facilisi cu ius, utroque moderatius voluptatibus no nam. Pri an graecis torquatos accommodare. Quodsi everti ei qui")
+print(regex_query(regex, test_str))
